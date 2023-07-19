d76d1467b59e4582a3690281d315d447bcbad38c
Mohammed Abdullah <mohammed.aladhary@outlook.com>
2023-07-19 11:48:06 +0400

    selectionSort

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace sortAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            List<int> list = new List<int> { 64, 25, 12, 22, 11 };
+
+            selectionSort.Sort(list);
+
+            foreach (int i in list)
+            {
+                Console.Write(i+ " ");
+            }
+        }
+    }
+}
---
+++ selectionSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sortAlgorithms
+{
+    internal class selectionSort
+    {
+        public static void Sort(List<int> list)
+        {
+            int n = list.Count;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (list[j] < list[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                // Swap the found minimum element with the first element
+                int temp = list[minIndex];
+                list[minIndex] = list[i];
+                list[i] = temp;
+            }
+        }
+    }
+}
